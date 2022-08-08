67bc181a698a69a770ac10370b7f8c7b82fd7d36
Omar Bittar <omar.bittar@tuhh.de>
2022-08-08 09:31:39 +0200

    work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-import {
-	SunIcon,
-	MoonIcon,
-	HomeIcon,
-	MailIcon,
-	UserIcon,
-} from "@heroicons/react/solid";
+import { SunIcon, MoonIcon, HomeIcon } from "@heroicons/react/solid";
 
 import { useState } from "react";
 
@@ -44,13 +38,13 @@ function DarkModeToggler({ className }) {
 
 function Navbar() {
 	return (
-		<nav className='z-20 fixed top-0 w-screen border-b h-12 shadow-xl'>
+		<nav className='z-20 fixed top-0 w-screen border-b shadow-xl'>
 			<div className='max-w-7xl w-full h-full items-center flex mx-auto px-2 justify-between'>
 				<div className='flex items-center'>
 					<h1 className='border-r px-2 mr-2 md:mr-5 md:px-5 md:border-l'>
 						src-yahiko
 					</h1>
-					<HomeIcon className='w-12 p-3' />
+					<HomeIcon className='h-12 p-3' />
 					{/* <UserIcon className='w-12 p-3' /> */}
 					{/* <MailIcon className='w-12 p-3' /> */}
 				</div>
@@ -64,9 +58,9 @@ function Navbar() {
 
 function Sidebar() {
 	return (
-		<aside className='z-10 fixed bottom-0 w-screen h-12 md:left-0 md:h-screen border-t md:border-r md:w-12 shadow-xl'>
-			<div className='w-full h-full items-center flex md:flex-col p-2 justify-center'>
-				{/* <SunIcon className='w-12 p-2' /> */}
+		<aside className='z-10 fixed bottom-0 w-screen md:left-0 md:h-screen border-t md:border-r md:w-auto shadow-xl'>
+			<div className='w-full h-full items-center flex md:flex-col justify-center'>
+				<SunIcon className='w-14 p-3 scale-0' />
 				{/* <SunIcon className='w-12 p-2' /> */}
 			</div>
 		</aside>
@@ -90,6 +84,22 @@ function App() {
 					<h1 className='border-b text-center pb-2 mb-2 uppercase tracking-wider text-4xl'>
 						Work in progress
 					</h1>
+					<div className='opacity-30'>
+						<div className='w-full h-32 border bg-neutral-500 rounded-xl shadow-sm my-5'></div>
+						<div className='flex space-x-5'>
+							<div className='w-full h-32 border bg-neutral-500 rounded-xl shadow-sm'></div>
+							<div className='w-full h-32 border bg-neutral-500 rounded-xl shadow-sm'></div>
+						</div>
+						<div className='w-full h-32 border bg-neutral-500 rounded-xl shadow-sm my-5'></div>
+
+						<div className='flex space-x-5'>
+							<div className='flex flex-1 flex-col space-y-5'>
+								<div className='w-full h-32 border bg-neutral-500 rounded-xl shadow-sm'></div>
+								<div className='w-full h-32 border bg-neutral-500 rounded-xl shadow-sm'></div>
+							</div>
+							<div className='flex-1 w-full h-69 border bg-neutral-500 rounded-xl shadow-sm'></div>
+						</div>
+					</div>
 				</div>
 			</Container>
 		</div>
