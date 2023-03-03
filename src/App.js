f32c351d800c4572ccedf08405d271b8ef8ec8ee
Omar Bittar <omar.bittar@bitrate-it.de>
2023-03-03 16:35:16 +0100

    images fix

--- src/App.js
+++ src/App.js
@@ -492,8 +492,8 @@ function App() {
 							</button>
 						</ShowcaseCard>
 						<ShowcaseCard
-							logo="/img/bitratelogo2.png"
-							img="/img/bitratelogo.png"
+							logo="/img/bitrate_klein.png"
+							img="/img/bitrate.png"
 							title="Bitrate IT"
 							content="Bitrate IT Solution Architects: Website für geschäftliche Anfragen"
 						>              <button
@@ -508,14 +508,14 @@ function App() {
 								Öffnen
 							</button></ShowcaseCard>
 						<ShowcaseCard
-							logo="/img/bitratelogo2.png"
-							img="/img/bitratelogo.png"
+							logo="/img/bitrate_klein.png"
+							img="/img/bitrate.png"
 							title="Title"
 							content="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
 						></ShowcaseCard>
 						<ShowcaseCard
-							logo="/img/bitratelogo2.png"
-							img="/img/bitratelogo.png"
+							logo="/img/bitrate_klein.png"
+							img="/img/bitrate.png"
 							title="Title"
 							content="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
 						></ShowcaseCard>
